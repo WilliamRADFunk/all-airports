ff83d2996047b78c92a342c7eeb67cf3e3a198bf
WilliamRADFunk <CONTACT@WILLIAMRADFUNK.com>
2019-05-29 17:13:20 -0400

    Corrects some n-triples formatting issues.

--- src/utils/save-file.ts
+++ src/utils/save-file.ts
@@ -70,28 +70,28 @@ export function saveFile(fileName: string, storeName: string, context: string): 
 function convertJsonldToNTriples(): void {
 	const length = store.jsonLD.length;
 	for (let i = 0; i < length; i++) {
-		const entity = store.jsonLD.pop();
+		let entity = store.jsonLD.pop();
 		if (entity) {
 			const mainId = entity['@id'];
 			const mainLabel = entity['http://www.w3.org/2000/01/rdf-schema#label'];
 			const mainType = entity['@type'];
-			store.jsonNT += `<${mainId}> <http://www.w3.org/2000/01/rdf-schema#label> ${mainLabel} .\n`;
-			store.jsonNT += `<${mainId}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ${mainType} .\n`;
+			store.jsonNT += `<${mainId}> <http://www.w3.org/2000/01/rdf-schema#label> ${JSON.stringify(mainLabel)} .\n`;
+			store.jsonNT += `<${mainId}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <${mainType}> .\n`;
 			Object.entries(entity).forEach(entry => {
 				if (['@id', '@type', 'http://www.w3.org/2000/01/rdf-schema#label'].includes(entry[0])) {
 					// Taken care of already.
 				} else if (Array.isArray(entry[1])) {
 					entry[1].forEach(innerEntry => {
 						store.jsonNT += `<${mainId}> <${entry[0]}> <${innerEntry['@id']}> .\n`;
-						store.jsonNT += `<${innerEntry['@id']}> <http://www.w3.org/2000/01/rdf-schema#label> ${innerEntry['http://www.w3.org/2000/01/rdf-schema#label']} .\n`;
-						store.jsonNT += `<${innerEntry['@id']}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ${innerEntry['@type']} .\n`;
+						store.jsonNT += `<${innerEntry['@id']}> <http://www.w3.org/2000/01/rdf-schema#label> ${JSON.stringify(innerEntry['http://www.w3.org/2000/01/rdf-schema#label'])} .\n`;
+						store.jsonNT += `<${innerEntry['@id']}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <${innerEntry['@type']}> .\n`;
 					});
 				} else if(typeof entry[1] === 'object') {
 					store.jsonNT += `<${mainId}> <${entry[0]}> <${entry[1]['@id']}> .\n`;
-					store.jsonNT += `<${entry[1]['@id']}> <http://www.w3.org/2000/01/rdf-schema#label> ${entry[1]['http://www.w3.org/2000/01/rdf-schema#label']} .\n`;
-					store.jsonNT += `<${entry[1]['@id']}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ${entry[1]['@type']} .\n`;
+					store.jsonNT += `<${entry[1]['@id']}> <http://www.w3.org/2000/01/rdf-schema#label> ${JSON.stringify(entry[1]['http://www.w3.org/2000/01/rdf-schema#label'])} .\n`;
+					store.jsonNT += `<${entry[1]['@id']}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <${entry[1]['@type']}> .\n`;
 				} else {
-					store.jsonNT += `<${mainId}> <${entry[0]}> ${entry[1]} .\n`;
+					store.jsonNT += `<${mainId}> <${entry[0]}> ${JSON.stringify(entry[1])} .\n`;
 				}
 			});
 		}
